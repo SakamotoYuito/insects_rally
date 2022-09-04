37587d686f7a971f095f9bc6c67cc3bee3f970ff
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-09-04 15:13:07 +0900

    first commit

---
+++ firebase/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app'
+
+// FirebaseApp インスタンスを初期化する
+initializeApp({
+  apiKey: "AIzaSyDUyPgNlr4K6cP_R0skzOtnW-BawgdOKE0",
+  authDomain: "insect-rally.firebaseapp.com",
+  projectId: "insect-rally",
+  storageBucket: "insect-rally.appspot.com",
+  messagingSenderId: "635528997469",
+  appId: "1:635528997469:web:4bfbd3f0ebf51e29e8bbb4"
+})
