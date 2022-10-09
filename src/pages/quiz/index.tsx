0ce70f910662f1af00754de8c723a2c6f5b5bdc3
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2022-10-10 08:51:11 +0900

    クイズページとデータベースを連携

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -4,25 +4,39 @@ import Image from "next/image";
 import Layout from "pages/layout";
 import MyRadioForm from "components/Form/RadioForm";
 import MyTextForm from "components/Form/TextForm";
-import useGetQuiz from "./hook";
 import Check from "assets/icon/check.svg";
 import Cross from "assets/icon/cross.svg";
 import styles from "./style.module.scss";
+import { GetServerSideProps } from "next";
+import { adminDB } from "utils/server";
+import Button from "react-bootstrap/Button";
 
-const Quiz = () => {
+type Props = {
+  quiz: QuizDataForDisplay;
+};
+
+type QuizDataForDisplay = {
+  type: "radioImage" | "radioText" | "form";
+  sentence: string;
+  choices: string[];
+  answer: number;
+};
+
+const Quiz = (props: Props) => {
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
   const [isAnswered, setIsAnswered] = useState(false);
   const [validated, setValidated] = useState(false);
   const [userAnswer, setUserAnswer] = useState<string | number>("");
 
-  const router = useRouter();
-  const quizNumber = router.query.id;
-  const type = "Radio";
-  const sentence = useGetQuiz();
-  const labels = ["オス", "メス"];
   const buttonStr = "回答する";
-  const correctNumber = 1;
-  const correctAnswer = labels[correctNumber - 1];
+  const router = useRouter();
+
+  const quizNumber = router.query.id;
+  const type = props.quiz.type;
+  const sentence = props.quiz.sentence;
+  const choices = props.quiz.choices;
+  const correctNumber = props.quiz.answer;
+  const correctAnswer = choices[correctNumber - 1];
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     const form = e.currentTarget;
@@ -50,46 +64,82 @@ const Quiz = () => {
         </div>
       </div>
       <p className={styles.sentence}>{sentence}</p>
-      {type === "Radio" && isCorrect === null && isAnswered === false && (
+      {type === "radioText" && isCorrect === null && isAnswered === false && (
         <MyRadioForm
-          labels={labels}
+          labels={choices}
           buttonStr={buttonStr}
           onSubmit={(e) => handleSubmit(e)}
           answer={userAnswer}
           onChange={(e) => setUserAnswer(e.currentTarget.value)}
         />
       )}
-      {type !== "Radio" && isCorrect === null && isAnswered === false && (
+      {type === "form" && isCorrect === null && isAnswered === false && (
         <MyTextForm
           validated={validated}
-          label="ユーザーID"
+          label="回答"
           type="text"
-          placeHolder="ユーザーID"
-          feedback="ユーザーIDを入力してください"
+          placeHolder="回答"
+          feedback="回答を入力してください"
           onSubmit={(e) => {
             e !== undefined ? handleSubmit(e) : null;
           }}
           onChange={(e) => {
             e !== undefined ? setUserAnswer(e.currentTarget.value) : null;
           }}
-          buttonStr="ログイン"
+          buttonStr="回答する"
           error="もう一度入力してください"
         />
       )}
       {isCorrect === true && isAnswered === true && (
-        <div className={styles.correct}>
-          <p className={styles.announce}>正解です</p>
-          <h2 className={styles.answer}>正解：{correctAnswer}</h2>
-        </div>
+        <>
+          <div className={styles.correct}>
+            <p className={styles.announce}>正解です</p>
+            <h2 className={styles.answer}>正解：{correctAnswer}</h2>
+          </div>
+          <div className={styles.button}>
+            <Button>報酬を受け取る</Button>
+          </div>
+        </>
       )}
       {isCorrect === false && isAnswered === true && (
-        <div className={styles.incorrect}>
-          <p className={styles.announce}>ざんねん…不正解です…</p>
-          <h2 className={styles.answer}>正解：{correctAnswer}</h2>
-        </div>
+        <>
+          <div className={styles.incorrect}>
+            <p className={styles.announce}>ざんねん…不正解です…</p>
+            <h2 className={styles.answer}>正解：{correctAnswer}</h2>
+          </div>
+          <div className={styles.button}>
+            <Button>ホームへ戻る</Button>
+          </div>
+        </>
       )}
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const quizDataForDisplay: QuizDataForDisplay = {
+    type: "radioText",
+    sentence: "",
+    choices: [],
+    answer: 0,
+  };
+  const quizId = Number(context.query.id);
+  const querySnapshot = await adminDB
+    .collection("quiz")
+    .where("quiz.id", "==", quizId)
+    .get();
+  querySnapshot.forEach((doc) => {
+    const quizData = doc.data().quiz;
+    quizDataForDisplay.type = quizData.type;
+    quizDataForDisplay.sentence = quizData.sentence;
+    quizDataForDisplay.choices = quizData.choices;
+    quizDataForDisplay.answer = quizData.answer;
+  });
+  return {
+    props: {
+      quiz: quizDataForDisplay,
+    },
+  };
 };
 
 export default Quiz;
